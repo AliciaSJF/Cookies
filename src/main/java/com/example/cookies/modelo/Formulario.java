5d13e3847ed0cd06ce0c70ba9ca383f4511254b4
AliciaSJF <aliciasj.2003@gmail.com>
2023-12-19 10:13:12 +0100

    Sesiones

--- src/main/java/com/example/cookies/modelo/Formulario.java
+++ src/main/java/com/example/cookies/modelo/Formulario.java
@@ -1,4 +1,12 @@
 package com.example.cookies.modelo;
 
+import lombok.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@AllArgsConstructor@RequiredArgsConstructor
+@Setter @Getter @ToString
 public class Formulario {
+    private String nombre;
+    private String clave;
+
 }
